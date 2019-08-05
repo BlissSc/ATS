91f07348ea51413712639936d3532b0bfd234215
Bliss <31372684+BlissSc@users.noreply.github.com>
2019-08-05 13:50:44 -0300

    1

---
+++ simple_list.py
@@ -0,0 +1,10 @@
+# Im noob in python so I made this
+# print this wea
+import json
+
+# open data
+with open('data.json', 'r') as f:
+    data = json.load(f)
+    
+# print this wea
+    print(data)
